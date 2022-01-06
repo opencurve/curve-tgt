fd1130c68eeb698bd43b375590d8ff4ca0953f16
yfxu <yfxu@corp.netease.com>
2022-01-06 17:12:04 +0800

    use async threads to push requests to curve.

--- usr/bs_curve.c
+++ usr/bs_curve.c
@@ -40,23 +40,35 @@
 #include "target.h"
 #include "scsi.h"
 
+#define __predict_true(exp)     __builtin_expect((exp), 1)
+#define __predict_false(exp)    __builtin_expect((exp), 0)
+
 struct bs_curve_info {
 	int curve_fd;
 	int evt_fd;
 	pthread_mutex_t mutex;
-	pthread_rwlock_t close_lock;
 	struct list_head cmd_inflight_list;
 	struct list_head cmd_complete_list;
 	unsigned int ninflight;
 	unsigned int ncomplete;
 	struct scsi_lu *lu;
+
+	pthread_t *worker_thread;
+	int nr_worker_threads;
+	int stop;
+
+	/* workers sleep on this and signaled by tgtd */
+	pthread_cond_t pending_cond;
+	/* locked by tgtd and workers */
+	pthread_mutex_t pending_lock;
+	/* protected by pending_lock */
+	struct list_head cmd_pending_list;
 };
 
 typedef struct bs_curve_iocb {
 	struct NebdClientAioContext ctx;
 	struct scsi_cmd *cmd;
 	struct bs_curve_info *info;
-	struct list_head bs_list;
 
 	struct write_same {
 		int64_t tl;
@@ -64,64 +76,168 @@ typedef struct bs_curve_iocb {
 	}ws;
 } bs_curve_iocb_t;
 
+static void cmd_submit(struct bs_curve_info *info, struct scsi_cmd *cmd);
+static void cmd_done(struct bs_curve_info *info, struct scsi_cmd *cmd);
+
 static inline struct bs_curve_info *BS_CURVE_I(struct scsi_lu *lu)
 {
 	return (struct bs_curve_info *) ((char *)lu + sizeof(*lu));
 }
 
-static void bs_curve_enq_inflight_io(struct bs_curve_info *info,
-		bs_curve_iocb_t *iocb)
+static void *thread_cmd_worker(void *arg)
 {
-	list_add_tail(&iocb->bs_list, &info->cmd_inflight_list);
+	struct bs_curve_info *info = arg;
+	struct scsi_cmd *cmd;
+
+	while (1) {
+		pthread_mutex_lock(&info->pending_lock);
+		while (!info->stop && list_empty(&info->cmd_pending_list))
+			pthread_cond_wait(&info->pending_cond,
+					  &info->pending_lock);
+		if (__predict_false(info->stop != 0)) {
+			pthread_mutex_unlock(&info->pending_lock);
+			break;
+		}
+		cmd = list_first_entry(&info->cmd_pending_list,
+				       struct scsi_cmd, bs_list);
+		list_del(&cmd->bs_list);
+		pthread_mutex_unlock(&info->pending_lock);
+
+		cmd_submit(info, cmd);
+	}
+
+	pthread_exit(NULL);
+}
+
+static void thread_group_stop(struct bs_curve_info *info)
+{
+	pthread_mutex_lock(&info->pending_lock);
+	info->stop = 1;
+	pthread_cond_broadcast(&info->pending_cond);
+	pthread_mutex_unlock(&info->pending_lock);
+}
+
+static tgtadm_err thread_group_open(struct bs_curve_info *info, int nr_threads)
+{
+	int i, ret;
+
+	info->stop = 0;
+	info->worker_thread = zalloc(sizeof(pthread_t) * nr_threads);
+	if (!info->worker_thread)
+		return TGTADM_NOMEM;
+
+	for (i = 0; i < nr_threads; i++) {
+		ret = pthread_create(&info->worker_thread[i], NULL,
+				     thread_cmd_worker, info);
+
+		if (ret) {
+			eprintf("failed to create a worker thread, %d %s\n",
+				i, strerror(ret));
+			goto destroy_threads;
+		}
+	}
+	info->nr_worker_threads = nr_threads;
+
+	return TGTADM_SUCCESS;
+
+destroy_threads:
+	thread_group_stop(info);
+	for (; i > 0; i--) {
+		if (info->worker_thread[i - 1]) {
+			pthread_join(info->worker_thread[i - 1], NULL);
+			dprintf("stopped the worker thread %d\n", i - 1);
+		}
+	}
+
+	free(info->worker_thread);
+	info->worker_thread = NULL;
+	return TGTADM_NOMEM;
+}
+
+static void thread_group_close(struct bs_curve_info *info)
+{
+	int i;
+
+	thread_group_stop(info);
+	for (i = 0; i < info->nr_worker_threads && info->worker_thread[i]; i++)
+		pthread_join(info->worker_thread[i], NULL);
+	free(info->worker_thread);
+	info->worker_thread = NULL;
+	info->nr_worker_threads = 0;
+}
+
+static inline void bs_curve_enq_inflight_io(struct bs_curve_info *info,
+		struct scsi_cmd *cmd)
+{
+	list_add_tail(&cmd->bs_list, &info->cmd_inflight_list);
 	info->ninflight++;
 }
 
-static void bs_curve_deq_inflight_io(struct bs_curve_info *info,
-		bs_curve_iocb_t *iocb)
+static inline void bs_curve_deq_inflight_io(struct bs_curve_info *info,
+		struct scsi_cmd *cmd)
 {
-	list_del(&iocb->bs_list);
+	list_del(&cmd->bs_list);
 	info->ninflight--;
 }
 
-static void bs_curve_enq_complete_io(struct bs_curve_info *info,
-		bs_curve_iocb_t *iocb)
+static inline int bs_curve_enq_complete_io(struct bs_curve_info *info,
+		struct scsi_cmd *cmd)
 {
-	list_add_tail(&iocb->bs_list, &info->cmd_complete_list);
+	int empty = list_empty(&info->cmd_complete_list);
+	list_add_tail(&cmd->bs_list, &info->cmd_complete_list);
 	info->ncomplete++;
+	return empty;
 }
 
 /*
-static void bs_curve_deq_complete_io(struct bs_curve_info *info,
-		bs_curve_iocb_t *iocb)
+static inline void bs_curve_deq_complete_io(struct bs_curve_info *info,
+		struct scsi_cmd *cmd)
 {
-	list_del(&iocb->bs_list);
+	list_del(&cmd->bs_list);
 	info->ncomplete--;
 }
 */
 
+static inline void set_medium_error_r(int *result, uint8_t *key, uint16_t *asc)
+{
+	*result = SAM_STAT_CHECK_CONDITION;
+	*key = MEDIUM_ERROR;
+	*asc = ASC_READ_ERROR;
+}
+
+static inline void set_medium_error_w(int *result, uint8_t *key, uint16_t *asc)
+{
+	*result = SAM_STAT_CHECK_CONDITION;
+	*key = MEDIUM_ERROR;
+	*asc = ASC_WRITE_ERROR;
+}
+
+static void cmd_done(struct bs_curve_info *info, struct scsi_cmd *cmd)
+{
+	pthread_mutex_lock(&info->mutex);
+	bs_curve_deq_inflight_io(info, cmd);
+	int empty = bs_curve_enq_complete_io(info, cmd);
+	pthread_mutex_unlock(&info->mutex);
+
+	if (!empty)
+		return;
+
+	/* This is first entry, needs to signal other threads */
+	int64_t counter = 1;
+
+	while (write(info->evt_fd, &counter, 8) < 0) {
+		if (errno == EINTR)
+			continue;
+		break;
+	}
+}
+
 static void bs_curve_aio_callback(struct NebdClientAioContext* curve_ctx)
 {
 	bs_curve_iocb_t *iocb = container_of(curve_ctx, bs_curve_iocb_t, ctx);
-	struct bs_curve_info *info = iocb->info;
-	int64_t count = 1;
-
-	pthread_rwlock_rdlock(&info->close_lock);
-
-	pthread_mutex_lock(&info->mutex);
-	bs_curve_deq_inflight_io(info, iocb);
-	bs_curve_enq_complete_io(info, iocb);
-	pthread_mutex_unlock(&info->mutex);
-
-	write(info->evt_fd, &count, 8);
-
-	pthread_rwlock_unlock(&info->close_lock);
-}
-
-static void bs_curve_complete_one(struct bs_curve_info *info,
-		bs_curve_iocb_t *iocb)
-{
 	struct scsi_cmd *cmd = iocb->cmd;
-	int result = 0;
+	struct bs_curve_info *info = iocb->info;
+	int result = SAM_STAT_GOOD;
 	int asc = 0;
 
 	switch (cmd->scb[0]) {
@@ -139,12 +255,15 @@ static void bs_curve_complete_one(struct bs_curve_info *info,
 	if (iocb->ctx.ret == 0)
 		result = SAM_STAT_GOOD;
 	else {
-		sense_data_build(cmd, MEDIUM_ERROR, asc);
 		result = SAM_STAT_CHECK_CONDITION;
+		eprintf("io error %p %x %d %ld %" PRIu64 ", %m\n",
+                        cmd, cmd->scb[0], iocb->ctx.ret, iocb->ctx.length,
+			iocb->ctx.offset);
+		sense_data_build(cmd, MEDIUM_ERROR, asc);
 	}
 
-	dprintf("cmd: %p\n", cmd);
-	target_cmd_io_done(cmd, result);
+	scsi_set_result(cmd, result);
+	cmd_done(info, cmd);
 
 	free(iocb);
 }
@@ -153,7 +272,7 @@ static void bs_curve_get_completions(int fd, int events, void *data)
 {
 	struct bs_curve_info *info = data;
 	struct list_head temp_list;
-	struct bs_curve_iocb *iocb;
+	struct scsi_cmd *cmd;
 	/* read from eventfd returns 8-byte int, fails with the error EINVAL
 	   if the size of the supplied buffer is less than 8 bytes */
 	uint64_t evts_complete;
@@ -176,9 +295,10 @@ retry_read:
 	pthread_mutex_unlock(&info->mutex);
 
 	while (!list_empty(&temp_list)) {
-		iocb = list_first_entry(&temp_list, bs_curve_iocb_t, bs_list);
-		list_del(&iocb->bs_list);
-		bs_curve_complete_one(info, iocb);
+		cmd = list_first_entry(&temp_list, struct scsi_cmd, bs_list);
+		list_del(&cmd->bs_list);
+		dprintf("back to tgtd, %p\n", cmd);
+		target_cmd_io_done(cmd, scsi_get_result(cmd));
 	}
 }
 
@@ -220,15 +340,13 @@ static void bs_curve_aio_write_same_callback(struct NebdClientAioContext* curve_
 
 	if (iocb->ws.tl <= 0 || iocb->ctx.ret != 0) {
 stop:
-		pthread_rwlock_rdlock(&info->close_lock);
-
 		pthread_mutex_lock(&info->mutex);
-		bs_curve_deq_inflight_io(info, iocb);
-		bs_curve_enq_complete_io(info, iocb);
+		bs_curve_deq_inflight_io(info, cmd);
+		int empty = bs_curve_enq_complete_io(info, cmd);
 		pthread_mutex_unlock(&info->mutex);
-		write(info->evt_fd, &count, 8);
+		if (empty)
+			write(info->evt_fd, &count, 8);
 
-		pthread_rwlock_unlock(&info->close_lock);
 		dprintf("write same complete: %d\n", iocb->ctx.ret);
 	} else {
 		bs_curve_io_prep_write_same_2(info, iocb);
@@ -294,88 +412,104 @@ static int bs_curve_io_prep_write_same(struct bs_curve_info *info,
 	return 0;
 }
 
-static int bs_curve_cmd_submit(struct scsi_cmd *cmd)
+static void cmd_submit(struct bs_curve_info *info, struct scsi_cmd *cmd)
 {
-	enum IOMode { IO_NOOP, IO_READ, IO_WRITE, IO_WRITE_SAME } mode = IO_NOOP;
-	struct scsi_lu *lu = cmd->dev;
-	struct bs_curve_info *info = BS_CURVE_I(lu);
+	//struct scsi_lu *lu = cmd->dev;
 	bs_curve_iocb_t *iocb = NULL;
 	unsigned int scsi_op = (unsigned int)cmd->scb[0];
-	int err = 0;
+	int result = SAM_STAT_GOOD;
+	uint8_t key = 0;
+	uint16_t asc = 0;
+	int length = 0;
+
+	pthread_mutex_lock(&info->mutex);
+	bs_curve_enq_inflight_io(info, cmd);
+	pthread_mutex_unlock(&info->mutex);
 
 	switch (scsi_op) {
 	case WRITE_6:
 	case WRITE_10:
 	case WRITE_12:
 	case WRITE_16:
-		mode = IO_WRITE;
+		length = scsi_get_out_length(cmd);
+		iocb = calloc(1, sizeof(bs_curve_iocb_t));
+		iocb->cmd = cmd;
+		iocb->info = info;
+		bs_curve_io_prep_pwrite(info, iocb);
+		if (nebd_lib_aio_pwrite(info->curve_fd, &iocb->ctx)) {
+			set_medium_error_w(&result, &key, &asc);
+			break;
+		}
 		break;
 	case READ_6:
 	case READ_10:
 	case READ_12:
 	case READ_16:
-		mode = IO_READ;
+		length = scsi_get_in_length(cmd);
+		iocb = calloc(1, sizeof(bs_curve_iocb_t));
+		iocb->cmd = cmd;
+		iocb->info = info;
+		bs_curve_io_prep_pread(info, iocb);
+		if (nebd_lib_aio_pread(info->curve_fd, &iocb->ctx)) {
+			set_medium_error_r(&result, &key, &asc);
+			break;
+		}
 		break;
-
 	case WRITE_SAME:
 	case WRITE_SAME_16:
 		/* WRITE_SAME used to punch hole in file */
 		if (cmd->scb[1] & 0x08) {
 			eprintf("get WRITE_SAME with unmap!");
-			return 0;
+			set_medium_error_w(&result, &key, &asc);
+			break;
 		}
+
 		dprintf("got WRITE_SAME command, offset=%ld bufsz=%d tl=%d\n",
 			 cmd->offset, scsi_get_out_length(cmd), cmd->tl);
-		mode = IO_WRITE_SAME;
+
+		length = cmd->tl;
+		iocb = calloc(1, sizeof(bs_curve_iocb_t));
+		iocb->cmd = cmd;
+		iocb->info = info;
+		bs_curve_io_prep_write_same(info, iocb);
+		if (nebd_lib_aio_pwrite(info->curve_fd, &iocb->ctx)) {
+			set_medium_error_w(&result, &key, &asc);
+			break;
+		}
+
 		break;
 	case SYNCHRONIZE_CACHE:
 	case SYNCHRONIZE_CACHE_16:
 	default:
 		dprintf("skipped cmd:%p op:%x\n", cmd, scsi_op);
-		return 0;
+		scsi_set_result(cmd, result);
+		cmd_done(info, cmd);
+		return;
 	}
 
-	iocb = calloc(1, sizeof(bs_curve_iocb_t));
-	iocb->cmd = cmd;
-	iocb->info = info;
-	pthread_mutex_lock(&info->mutex);
-	bs_curve_enq_inflight_io(info, iocb);
-	pthread_mutex_unlock(&info->mutex);
-
-	switch(mode) {
-	case IO_READ:
-		bs_curve_io_prep_pread(info, iocb);
-		if (nebd_lib_aio_pread(info->curve_fd, &iocb->ctx)) {
-			err = -1;
-		}
-		break;
-	case IO_WRITE:
-		bs_curve_io_prep_pwrite(info, iocb);
-		if (nebd_lib_aio_pwrite(info->curve_fd, &iocb->ctx)) {
-			err = -1;
-		}
-		break;
-	case IO_WRITE_SAME:
-		bs_curve_io_prep_write_same(info, iocb);
-		if (nebd_lib_aio_pwrite(info->curve_fd, &iocb->ctx)) {
-			err = -1;
-		}
-		break;
-	default:
-		err = -1;
-		eprintf("%s:%d, Invalid mode %d\n", __FILE__, __LINE__, mode);
-		abort();
-	}
-
-	if (err) {
-		pthread_mutex_lock(&info->mutex);
-		bs_curve_deq_inflight_io(info, iocb);
-		pthread_mutex_unlock(&info->mutex);
+	if (__predict_false(result != SAM_STAT_GOOD)) {
+		eprintf("io error %p %x %d %d %" PRIu64 ", %m\n",
+			cmd, cmd->scb[0], result, length, cmd->offset);
+		scsi_set_result(cmd, result);
+		sense_data_build(cmd, key, asc);
+		cmd_done(info, cmd);
 		free(iocb);
-	} else {
-		set_cmd_async(cmd);
 	}
-	return err;
+}
+
+static int bs_curve_cmd_submit(struct scsi_cmd *cmd)
+{
+	struct scsi_lu *lu = cmd->dev;
+	struct bs_curve_info *info = BS_CURVE_I(lu);
+
+	set_cmd_async(cmd);
+
+	pthread_mutex_lock(&info->pending_lock);
+	list_add_tail(&cmd->bs_list, &info->cmd_pending_list);
+	pthread_cond_signal(&info->pending_cond);
+	pthread_mutex_unlock(&info->pending_lock);
+
+	return 0;
 }
 
 static int bs_curve_open(struct scsi_lu *lu, char *path, int *fd, uint64_t *size)
@@ -421,11 +555,17 @@ static int bs_curve_open(struct scsi_lu *lu, char *path, int *fd, uint64_t *size
 	 * to chunk servers will be persisted on disk immediately.
 	 */
 	lu->attrs.dpofua = 1;
+
+	ret = thread_group_open(info, 1);
+	if (ret != 0)
+		goto delete_event;
 	return 0;
 
+delete_event:
 	tgt_event_del(afd);
 close_eventfd:
 	close(afd);
+	info->evt_fd = -1;
 close_curve:
 	nebd_lib_close(info->curve_fd);
 	info->curve_fd = -1;
@@ -436,9 +576,14 @@ static void bs_curve_close(struct scsi_lu *lu)
 {
 	struct bs_curve_info *info = BS_CURVE_I(lu);
 
+	thread_group_close(info);
 	if (info->curve_fd >= 0) {
 		nebd_lib_close(info->curve_fd);
 		info->curve_fd = -1;
+	}
+	if (info->evt_fd >= 0) {
+		tgt_event_del(info->evt_fd);
+		info->evt_fd = -1;
 	}
 }
 
@@ -468,13 +613,16 @@ static tgtadm_err bs_curve_init(struct scsi_lu *lu, char *bsopts)
 		return TGTADM_UNKNOWN_ERR;
 
 	memset(info, 0, sizeof(*info));
-	pthread_mutex_init(&info->mutex, NULL);
-	pthread_rwlock_init(&info->close_lock, NULL);
 	info->curve_fd = -1;
 	info->evt_fd = -1;
+	pthread_mutex_init(&info->mutex, NULL);
 	INIT_LIST_HEAD(&info->cmd_inflight_list);
 	INIT_LIST_HEAD(&info->cmd_complete_list);
 	info->lu = lu;
+
+	pthread_mutex_init(&info->pending_lock, NULL);
+	pthread_cond_init(&info->pending_cond, NULL);
+	INIT_LIST_HEAD(&info->cmd_pending_list);
 	return TGTADM_SUCCESS;
 }
 
@@ -482,11 +630,11 @@ static void bs_curve_exit(struct scsi_lu *lu)
 {
 	struct bs_curve_info *info = BS_CURVE_I(lu);
 
-	pthread_rwlock_wrlock(&info->close_lock);
-	close(info->evt_fd);
-	pthread_rwlock_unlock(&info->close_lock);
-	pthread_rwlock_destroy(&info->close_lock);
+	if (info->evt_fd >= 0)
+		close(info->evt_fd);
 	pthread_mutex_destroy(&info->mutex);
+	pthread_mutex_destroy(&info->pending_lock);
+	pthread_cond_destroy(&info->pending_cond);
 }
 
 static struct backingstore_template curve_bst = {
@@ -539,3 +687,4 @@ void register_bs_module(void)
 	bs_create_opcode_map(&curve_bst, opcodes, ARRAY_SIZE(opcodes));
 	register_backingstore_template(&curve_bst);
 }
+
